4436dc306dee7a0bda94167b14f6970a973d4902
Joseph Tastet <Josephwtastet@gmail.com>
2021-01-14 22:26:06 -0500

    Initial commit

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,38 @@
 class Api::ProductsController < ApplicationController
-  def all_products
-    @all_products = Product.all
-    render "products_views.json.jb"
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new({
+      name: params["name"],
+      price: params["price"].to_i,
+      image_url: params["image_url"],
+      description: params["description"],
+    })
+    @product.save
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    @products = Product.all
+    render "index.json.jb"
   end
 end
