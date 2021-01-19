d55d983c33356772c3136592c5d8aed69251dfcd
Joseph Tastet <Josephwtastet@gmail.com>
2021-01-18 19:17:54 -0500

    Oops, Added a lot and havent pushed it up in a while

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -26,6 +26,7 @@ class Api::ProductsController < ApplicationController
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
+    @product.save
     render "show.json.jb"
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    self.price <= 10
+  end
+
+  def tax
+    tax_price = self.price.to_f * 0.09
+  end
+
+  def total
+    total = self.price.to_f + self.tax
+  end
 end
