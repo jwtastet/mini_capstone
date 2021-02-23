fb40df50fb9143860c676a79069e21d37caf40f7
Joseph Tastet <Josephwtastet@gmail.com>
2021-02-22 19:32:39 -0500

    commit before heroku

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -43,6 +43,7 @@ class Api::ProductsController < ApplicationController
       description: params["description"],
     })
     if @product.save
+      Image.create(product_id: @product.id, url: params[:image_url])
       render "show.json.jb"
     else
       render json: { errors: @product.errors.full_messages }, status: 422
--- app/models/product.rb
+++ app/models/product.rb
@@ -35,6 +35,14 @@ class Product < ApplicationRecord
     CategoryProduct.where(product_id: self.id)
   end
 
+  def primary_image_url
+    if images.length > 0
+      images[0].url
+    else
+      "https://www.google.com/search?q=no+image+icon&rlz=1C5CHFA_enUS935US935&sxsrf=ALeKk02oJ964UmjbhbqDZayFX-AODw1h6Q:1613440184121&tbm=isch&source=iu&ictx=1&fir=srhpoiIpowpbOM%252CO0ee8H4WL2H1wM%252C_&vet=1&usg=AI4_-kQ3Nv4e6gYXelbtwc8unymkyAOaFQ&sa=X&ved=2ahUKEwiKnqmIpe3uAhUDFFkFHUYIAgcQ9QF6BAgKEAE#imgrc=srhpoiIpowpbOM"
+    end
+  end
+
   scope :discount, -> { where("price < 20") }
 
   scope :name_search, ->(search) { where("name ILIKE ?", "%#{search}%") }
